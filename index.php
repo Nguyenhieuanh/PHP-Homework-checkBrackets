d8576f256fd2f164a53eb119c60c9d03d60d4c62
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-04-20 12:56:09 +0700

    update

--- index.php
+++ index.php
@@ -1,36 +1,46 @@
 <?php
 include_once "Classes/Stack.php";
 
-function checkBrackets($string)
+function checkBalancesBrackets($string)
 {
-    $stack = new Stack();
+    $stack = new SplStack();
     for ($i = 0; $i < strlen($string); $i++) {
         switch ($string[$i]) {
             case '(':
                 $stack->push('parenthesis');
                 break;
             case ')':
-                if ($stack->pop() !== 'parenthesis') {
+                if ($stack->isEmpty()) {
                     return false;
+                } elseif (!$stack->isEmpty()) {
+                    if ($stack->pop() != 'parenthesis') {
+                        return false;
+                    }
                 }
                 break;
             case '[':
                 $stack->push('square brackets');
                 break;
             case ']':
-                if ($stack->pop() !== 'square brackets') {
+                if ($stack->isEmpty()) {
                     return false;
+                } elseif (!$stack->isEmpty()) {
+                    if ($stack->pop() != 'square brackets') {
+                        return false;
+                    }
                 }
                 break;
             case '{':
                 $stack->push('braces');
                 break;
             case '}':
-                if ($stack->pop() !== 'braces') {
+                if ($stack->isEmpty()) {
                     return false;
+                } elseif (!$stack->isEmpty()) {
+                    if ($stack->pop() != 'braces') {
+                        return false;
+                    }
                 }
-                break;
-            default:
                 break;
         }
     }
@@ -42,6 +52,6 @@ $str2 = '7 - 3 / 5 * ( 6 +4 ))';
 $str3 = '({ (4 - 6) * 7)';
 
 echo "<pre>";
-var_dump(checkBrackets($str1));
-var_dump(checkBrackets($str2));
-var_dump(checkBrackets($str3));
+var_dump(checkBalancesBrackets($str1));
+var_dump(checkBalancesBrackets($str2));
+var_dump(checkBalancesBrackets($str3));
